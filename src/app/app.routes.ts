a2d31ea765bb73f1f7e1d863d57d75007c98ead2
Nathandiv <k.n.hlungwani@gmail.com>
2024-10-24 14:00:56 +0200

    added routings to all pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { DashbordComponent } from './Pages/dashbord/dashbord.component';
+import { PackageComponent } from './Pages/package/package.component';
+import { ClientComponent } from './Pages/client/client.component';
+import { RoomsComponent } from './Pages/rooms/rooms.component';
+import { BookingComponent } from './Pages/booking/booking.component';
+import { UsersComponent } from './Pages/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'Dashbord',component:DashbordComponent},
+    {path:'Package',component:PackageComponent},
+    {path:'Client',component:ClientComponent},
+    {path:'Rooms',component:RoomsComponent},
+    {path:'Booking',component:BookingComponent},
+    {path:'Users',component:UsersComponent},
+
+];
