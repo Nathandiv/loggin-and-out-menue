31d3c4d810a884a0647b7e0df0dd0c66fd72c5b8
Nathandiv <k.n.hlungwani@gmail.com>
2024-10-25 16:10:38 +0200

    made changes

---
+++ src/app/admin-layer/admin-layer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-layer',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-layer.component.html',
+  styleUrl: './admin-layer.component.css'
+})
+export class AdminLayerComponent {
+
+}
